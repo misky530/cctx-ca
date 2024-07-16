1f5b46be70fe71ef61458aec6503bccf7f501363
caiqian <zero530@126.com>
2024-07-16 16:39:40 +0800

    通过代理获取binance和coinbase

--- diff/fetch.py
+++ diff/fetch.py
@@ -1,6 +1,26 @@
 import ccxt
 
-if __name__ == '__main__':
+
+def fetch_doge_price():
+    try:
+        # 创建一个 Coinbase 交易所对象
+        exchange = ccxt.coinbase({
+            'proxies': {
+                'http': 'http://127.0.0.1:7890',
+                'https': 'http://127.0.0.1:7890',
+            },
+        })
+
+        # 获取市场行情
+        ticker = exchange.fetch_ticker('DOGE/USD')
+
+        # 输出实时价格
+        print(f"DOGE/USD 最新价格: {ticker['last']}")
+    except Exception as e:
+        print(f"获取价格时出错: {e}")
+
+
+def fetch_doge_price_bin():
     # 创建一个 Binance 交易所对象
     # exchange = ccxt.binance()
     exchange = ccxt.binance({
@@ -15,3 +35,8 @@ if __name__ == '__main__':
 
     # 输出实时价格
     print(f"DOGE/USDT 最新价格: {ticker['last']}")
+
+
+if __name__ == '__main__':
+    fetch_doge_price()
+    fetch_doge_price_bin()
